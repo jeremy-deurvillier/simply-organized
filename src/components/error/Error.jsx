7c42e95a66c30cf6e1b4144321cb8738f207332b
jeremy-deurvillier <jerem.deurvil@gmail.com>
2023-12-01 15:25:20 +0100

    Add Radix, reset.css and basic design for Error component

--- src/components/error/Error.jsx
+++ src/components/error/Error.jsx
@@ -1,15 +1,34 @@
-import { useRouteError } from "react-router-dom"
+import { Box, Em, Flex, Heading, Text } from "@radix-ui/themes"
+import { Link, useRouteError } from "react-router-dom"
+import '../../styles.css'
 
 export default function Error() {
     const error = useRouteError()
-    
+
     return (
-        <div id="error-page">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
-        </div>
+        <Flex direction='column' justify='between' className='viewport-height-full'>
+            <Flex justify='center' align='center' className='viewport-height-medium'>
+                LOGO
+            </Flex>
+            <Flex direction='column' justify='between' align='center' className='viewport-height-medium'>
+                <Box py='3'>
+                    <Heading as='h1' hidden>Simply Organized</Heading>
+                    <Heading as='h2'>Erreur</Heading>
+                </Box>
+                <Box>
+                    <Text as='p' align='center'>Oups ! Une erreur s'est produite.</Text>
+                    <Text as='p' align='center'>
+                        Essayez de revenir à la <Link to='/'>page d'accueil</Link>.
+                    </Text>
+                </Box>
+                <Box py='1'>
+                    <Text as='p' align='center'>
+                        <Em>
+                            Statut : {error.statusText.toLowerCase() || error.message.toLowerCase()}
+                        </Em>
+                    </Text>
+                </Box>
+            </Flex>
+        </Flex>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './components/routes/Routes'
+import '@radix-ui/themes/styles.css'
+import { Theme } from '@radix-ui/themes'
 
 ReactDOM.createRoot(document.getElementById('app')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Theme accentColor='orange'>
+      <RouterProvider router={router} />
+    </Theme>
   </React.StrictMode>,
 )
