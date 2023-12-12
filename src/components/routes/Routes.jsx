3e33869b9d828fb3fe4aee5a7ef415d14c771dbe
jeremy-deurvillier <jerem.deurvil@gmail.com>
2023-12-12 18:42:53 +0100

    Add Today component on Routes

--- src/components/routes/Routes.jsx
+++ src/components/routes/Routes.jsx
@@ -1,11 +1,11 @@
 import { Route, createRoutesFromElements, createBrowserRouter } from 'react-router-dom'
 import Layout from '../layout/Layout'
 import Error from '../error/Error'
-import App from '../app/App'
+import Today from '../today/Today'
 
 const routes = createRoutesFromElements(
     <Route path='/' element={<Layout />} errorElement={<Error />}>
-        <Route index element={<App />} />
+        <Route index element={<Today />} />
     </Route>
 )
 
