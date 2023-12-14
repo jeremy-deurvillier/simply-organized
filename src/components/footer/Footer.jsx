5fed478ec03230c883e5f33767f2b613da4d5765
jeremy-deurvillier <jerem.deurvil@gmail.com>
2023-12-14 18:24:56 +0100

    Add Footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+import './Footer.css'
+
+// Visible seulement sur écran > 960px
+export default function Footer() {
+  return (
+    <div className='footer'>Footer</div>
+  )
+}
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,15 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../header/Header'
+import Footer from '../footer/Footer'
 import './Layout.css'
 
 export default function Layout() {
     return (
         <>
             <Header />
+
             <main className='mainContent'>
                 <Outlet />
             </main>
-            <footer>Footer</footer>
+            
+            <Footer />
         </>
     )
 }
