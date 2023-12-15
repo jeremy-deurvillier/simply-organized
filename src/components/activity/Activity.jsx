d3685c0fccc2a7a4051969bef593c218356dedec
jeremy-deurvillier <jerem.deurvil@gmail.com>
2023-12-15 18:06:37 +0100

    Add basic style on Activity component

--- src/components/activity/Activity.jsx
+++ src/components/activity/Activity.jsx
@@ -3,8 +3,8 @@ import './Activity.css'
 export default function Activity({activity}) {
   return (
     <div className='activity'>
-        <p>{activity.name}</p>
-        <p>{activity.project.title}</p>
+        <p className='activityName'>{activity.name}</p>
+        <p className='projectName'>{activity.project.title}</p>
     </div>
   )
 }
