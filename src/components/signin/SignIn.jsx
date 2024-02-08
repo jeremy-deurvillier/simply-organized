8545ac6cd57febf3fad1901aac60276ceef7203f
jeremy-deurvillier <jerem.deurvil@gmail.com>
2024-02-08 18:25:20 +0100

    Update SignIn component for responsive

--- src/components/signin/SignIn.jsx
+++ src/components/signin/SignIn.jsx
@@ -2,19 +2,47 @@ import { Button, Flex, Heading, TextField } from "@radix-ui/themes";
 import HeaderLogo from "../header-logo/HeaderLogo";
 
 export default function SignIn() {
+    const signinDirection = {
+        initial: 'column',
+        sm: 'row'
+    }
+
+    const signinHeight = {
+        initial: '100%'
+    }
+
+    const contentWrapper = {
+        initial: '100%',
+        sm: '50%'
+    }
+
+    const formContentWidth = {
+        initial: 'max-content',
+        sm: '100%'
+    }
+
+    const formStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minWidth: '50%'
+    }
+
     return (<>
-        <HeaderLogo />
-        
-        <Flex direction="column" gap="3" p="3">
-            <Heading as="h3" align="center" color="orange">Inscription</Heading>
-            <form action="/signin" method="post">
-                <Flex direction="column" gap="3" mx="3" p="4">
-                    <TextField.Input placeholder="Votre pseudo ou e-mail" />
-                    <TextField.Input placeholder="Votre mot de passe" />
-                    <TextField.Input placeholder="Votre mot de passe" />
-                    <Button color="plum">M'inscrire</Button>
-                </Flex>
-            </form>
+        <Flex height={signinHeight} direction={signinDirection} >
+            <HeaderLogo />
+
+            <Flex direction="column" align="center" justify='center' height='100%' width={contentWrapper} gap="3" p="4">
+                <Heading as="h3" align="center" color="orange">Inscription</Heading>
+                <form action="/signin" method="post" style={formStyle}>
+                    <Flex direction="column" gap="3" pt="7" width={formContentWidth}>
+                        <TextField.Input placeholder="Votre pseudo ou e-mail" />
+                        <TextField.Input placeholder="Votre mot de passe" />
+                        <TextField.Input placeholder="Votre mot de passe" />
+                        <Button color="plum">M'inscrire</Button>
+                    </Flex>
+                </form>
+            </Flex>
         </Flex>
     </>)
 }
